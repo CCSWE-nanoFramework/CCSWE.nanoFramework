93f2dd2c0b37fbef143465a06befd7dc4d2fbf7b
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-02-01 17:45:40 -0800

    Add Webserver sample

---
+++ samples/CCSWE.nanoFramework.WebServer.Samples/ExampleMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using CCSWE.nanoFramework.WebServer.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CCSWE.nanoFramework.WebServer.Samples
+{
+    internal class ExampleMiddleware : IMiddleware
+    {
+        private readonly ILogger _logger;
+
+        // Middleware supports dependency injection
+        public ExampleMiddleware(ILogger logger)
+        {
+            _logger = logger;
+            _logger.LogInformation($"{nameof(ExampleMiddleware)}.ctor() called.");
+        }
+
+        public void Invoke(HttpContext context, RequestDelegate next)
+        {
+            // Implement your middleware logic here
+            _logger.LogInformation($"{nameof(ExampleMiddleware)}.Invoke() called.");
+
+            // Call the next middleware in the pipeline or short-circuit the pipeline by not calling next (ie: you handled the request)
+            next(context);
+        }
+    }
+}
---
+++ samples/CCSWE.nanoFramework.WebServer.Samples/Services/DataService.cs
@@ -0,0 +1,22 @@
+﻿namespace CCSWE.nanoFramework.WebServer.Samples.Services
+{
+    public interface IDataService
+    {
+        DataItem? GetData();
+        void SetValue(string value);
+    }
+
+    public class DataService : IDataService
+    {
+        private DataItem? _data;
+
+        public DataItem? GetData() => _data;
+
+        public void SetValue(string value) => _data = new DataItem {Value = value};
+    }
+
+    public class DataItem
+    {
+        public string? Value { get; set; }
+    }
+}
